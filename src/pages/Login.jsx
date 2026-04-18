67f816ee8b67206e9c6d442e3f02935faaf3e029
Leandro Conde <leandro27conde@gmail.com>
2026-04-18 09:16:59 -0300

    ajuste de pasta

---
+++ src/pages/Login.jsx
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+import { supabase } from "../services/supabase";
+
+export default function Login({ onLogin, irParaCadastro }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [erro, setErro] = useState("");
+  const [mostrarSenha, setMostrarSenha] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.add("login-page");
+  
+    return () => {
+      document.body.classList.remove("login-page");
+    };
+  }, []);
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    setLoading(true);
+    setErro("");
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password: senha
+    });
+
+    if (error) {
+      setErro("Email ou senha inválidos");
+      setLoading(false);
+      return;
+    }
+
+    onLogin(data.user);
+    setLoading(false);
+
+  }
+
+  return (
+    <form onSubmit={handleLogin} className="login-box">
+      <h2>Login de acesso</h2>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+
+<div className="input-senha">
+  <input
+    type={mostrarSenha ? "text" : "password"}
+    placeholder="Senha"
+    value={senha}
+    onChange={e => setSenha(e.target.value)}
+  />
+
+  <button
+    type="button"
+    onClick={() => setMostrarSenha(prev => !prev)}
+    className="eye-btn"
+  >
+    {mostrarSenha ? "🙈" : "👁️"}
+  </button>
+</div>
+
+      <button type="submit" disabled={loading}>
+        {loading ? "Entrando..." : "Entrar"}
+      </button>
+
+      <button 
+        type="button" 
+        onClick={irParaCadastro}
+        className="btn-secundario"
+        >
+        Criar conta
+        </button>
+
+      {erro && <p className="erro">{erro}</p>}
+    </form>
+  );
+}
---
+++ src/pages/Registro.jsx
@@ -0,0 +1,96 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../services/supabase";
+
+export default function Registro({ onRegistro, voltarLogin }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
+
+  const [erro, setErro] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [mostrarSenha, setMostrarSenha] = useState(false);
+  const [mostrarConfirmar, setMostrarConfirmar] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.add("login-page");
+    return () => document.body.classList.remove("login-page");
+  }, []);
+
+  async function handleRegistro(e) {
+    e.preventDefault();
+    setLoading(true);
+    setErro("");
+
+    if (senha !== confirmarSenha) {
+      setErro("As senhas não coincidem");
+      setLoading(false);
+      return;
+    }
+
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password: senha
+    });
+
+    if (error) {
+      setErro(error.message);
+      setLoading(false);
+      return;
+    }
+
+    onRegistro(data.user);
+    setLoading(false);
+  }
+
+  return (
+    <form onSubmit={handleRegistro} className="login-box">
+      <h2>Criar conta</h2>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+
+      {/* SENHA */}
+      <div className="input-senha">
+        <input
+          type={mostrarSenha ? "text" : "password"}
+          placeholder="Senha"
+          value={senha}
+          onChange={e => setSenha(e.target.value)}
+        />
+
+        <button type="button" onClick={() => setMostrarSenha(!mostrarSenha)}>
+          {mostrarSenha ? "🙈" : "👁️"}
+        </button>
+      </div>
+
+      {/* CONFIRMAR SENHA */}
+      <div className="input-senha">
+        <input
+          type={mostrarConfirmar ? "text" : "password"}
+          placeholder="Confirmar senha"
+          value={confirmarSenha}
+          onChange={e => setConfirmarSenha(e.target.value)}
+        />
+
+        <button type="button" onClick={() => setMostrarConfirmar(!mostrarConfirmar)}>
+          {mostrarConfirmar ? "🙈" : "👁️"}
+        </button>
+      </div>
+
+      <button type="submit" disabled={loading}>
+        {loading ? "Criando..." : "Cadastrar"}
+      </button>
+
+      <p onClick={voltarLogin} style={{ cursor: "pointer" }}>
+        Já tenho conta
+      </p>
+
+      {erro && <p className="erro">{erro}</p>}
+    </form>
+  );
+}
