4c747e15519426bf02de1be0e4e6658313db392e
Leandro Conde <leandro27conde@gmail.com>
2026-04-18 21:22:15 -0300

    ..

--- Principal/gerenciador-de-tarefa/src/services/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-
-const supabaseUrl = "https://vmglciwaonssdvlrbgqk.supabase.co";
-const supabaseKey = "sb_publishable_bAwzVjh0OjJxham8KFgijQ_ThAu1Mz9";
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
-
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    base: command === 'build'
-      ? '/Gerenciador-de-Tarefas-2.0/'
-      : '/'
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/Gerenciador-de-Tarefas-2.0/'
 })
